ac347050e18f8b62fbe8a6116ba1d097b8bd3ce9
Ayon Paul <ayon.paul@stonybrook.edu>
2022-06-02 14:17:00 -0400

    Working on building REST api

--- server/app.py
+++ server/app.py
@@ -1,21 +1,69 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import motor.motor_asyncio #Motor serves as a MongoDB driver: API that allows non blocking access to MongoDB
-
+from pydantic import BaseModel
+from typing import List
 app = FastAPI() #Creates an instance of an api to use
-database = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/').sports_trivia #Connecting to MongoDB..
+database = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/').st #Connecting to MongoDB..
+
+#from QuestionTR import Question
+
+users_coll = database.users
+questions_coll = database["questions"]
+set_coll = database.sets
+
+class Question(BaseModel):
+    question: str
+    answer: str 
+    username: str
+    difficulty: str
+    player: str 
+    tags: List[str] = []
+
 
 origins = [
     "https://localhost:8080"
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins = origins,
+#     allow_credentials = True,
+#     allow_methods=["*"],
+#     allow_headers=["*"]
+# )
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/questions/{question_id}")
+async def get_question(id):
+    try:
+        question = await questions_coll.find_one({"_id":id})
+        return question
+    except:
+        return "Question not found"
+
+@app.post("/questions/", response_model= Question)
+async def create_question(question: Question):
+    try:
+        newQuestion = await questions_coll.insert_one(question)
+        return newQuestion
+    except:
+        return None
+
+@app.delete("/questions/{question_id}", response_model = Question)
+async def delete_question(id):
+    question = await questions_coll.delete_one({"_id" : id})
+    return question
+
+@app.put("/questions/{question_id}", response_model=str)
+async def update_question(question: Question):
+    try:
+        update_question = await questions_coll.findOneAndUpdate({question._id},{question})
+        return "Question successfully updated"
+    except:
+        return "Question could not be updated"
+
+
+
--- server/databases/user.py
+++ server/databases/user.py
@@ -1,12 +1,9 @@
 from typing import List
-
-import strawberry
 from app import database
-from SetTR import QuestionSet
+from set import QuestionSet
 
 users_coll = database.users
 
-@strawberry.type
 class User:
     username: str
     email: str
