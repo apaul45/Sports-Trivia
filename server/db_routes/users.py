4e765b1b82ec26258793bd7f06d5b0f84843a0db
Ayon Paul <ayon.paul@stonybrook.edu>
2023-01-24 21:29:35 -0500

    Refactor users.py

--- server/db_routes/users.py
+++ server/db_routes/users.py
@@ -1,7 +1,6 @@
 #This is a basic authentication system using Oauth2 and JWTs
-from fastapi import Depends, APIRouter, HTTPException, Request, Response
+from fastapi import Depends, APIRouter, HTTPException, Response
 import sys
-
 from server.models import OAuth2PasswordBearerWithCookie
 sys.path.insert(0,"..")
 from server.app import users_coll
@@ -39,13 +38,6 @@ class RegisterUser(BaseModel):
 class Token(BaseModel):
     access_token: str
 
-@router.get("/users")
-async def get_all_users():
-    #Use {_id: 0} to ignore the _id field when requesting: this is to prevent issues regarding ObjectIds in MongoDB
-    #Adding {username: 1} allows for only returning the username of each document 
-    users = await users_coll.find({},{'username': 1, '_id': 0}).to_list(length=None)
-    return users
-
 #From FastAPI documentation: this will serve as authorization for all routes
 #Once the user authenticates, oauth2_scheme will check for their token, which is why its a dependency
 async def get_current_user(token: str = Depends(oauth2_scheme)):
@@ -67,6 +59,13 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         raise credentials_exception
     return user
 
+@router.get("/users")
+async def get_all_users():
+    #Use {_id: 0} to ignore the _id field when requesting: this is to prevent issues regarding ObjectIds in MongoDB
+    #Adding {username: 1} allows for only returning the username of each document 
+    users = await users_coll.find({},{'username': 1, '_id': 0}).to_list(length=None)
+    return users
+
 #A user can register, but they must login after to receive a token (PasswordBearer only supports one url)
 @router.post("/register")
 async def register_user(user: RegisterUser):
@@ -78,15 +77,18 @@ async def register_user(user: RegisterUser):
         user.password = pwd_context.hash(user.password)
         user = jsonable_encoder(user)
         del user["password_confirmed"]
+
         user = await users_coll.insert_one(user)
-        if user:
-            return {"msg": "user successfully registered!"}
-        raise HTTPException(status_code=400, detail="An error occurred while trying to register this user")
+
+        if not user:
+            raise HTTPException(status_code=400, detail="An error occurred while trying to register this user")
+        
+        return {"msg": "user successfully registered!"}
     
 #OAuth2PasswordRequestForm is a dependency that creates a form with the entered username and password
 #Since this routes path is "/token", it will be called once the user enters their info into the request form
 @router.post("/login", response_model=Token)
-async def login_user(response: Response ,form_data: OAuth2PasswordRequestForm = Depends()):
+async def login_user(response: Response, form_data: OAuth2PasswordRequestForm = Depends()):
     user = await users_coll.find_one({"username": form_data.username})
     
     if not user:
@@ -95,8 +97,9 @@ async def login_user(response: Response ,form_data: OAuth2PasswordRequestForm = 
         raise HTTPException(status_code=400, detail="Incorrect password")
 
     #Create and send a JWT for the user to use for authorization
-    token = jwt.encode({"user": user["username"]}, secret_key, algorithm= algorithm)
+    token = jwt.encode({"user": user["username"]}, secret_key, algorithm=algorithm)
     response.set_cookie(key="access_token",value=f"Bearer {token}", httponly=True, secure=True, samesite='none')  #set HttpOnly cookie in response
+    
     return {"access_token": token, "token_type": "bearer"}
 
 @router.post("/logout")
